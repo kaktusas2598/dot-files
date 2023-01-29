c3929741455bfa571f311ae250b01e1412ebd3e8
Nerijus Vilčinskas <nerijus.vilcinskas@gmail.com>
2023-01-29 00:58:08 +0000

    Add nvim transparency

--- nvim/lua/user/colorscheme.lua
+++ nvim/lua/user/colorscheme.lua
@@ -1,4 +1,9 @@
 --vim.cmd('colorscheme deep-space')
 --vim.cmd('colorscheme ayu')
 vim.cmd('colorscheme onedark')
+
+-- For transparency
+vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"})
+
 vim.cmd('set termguicolors')
