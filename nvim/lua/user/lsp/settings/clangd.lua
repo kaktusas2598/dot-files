4ad8e9e1b3661b7f6ab3103bd82dda5e00fcd137
Nerijus Vilčinskas <nerijus.vilcinskas@gmail.com>
2023-01-30 00:05:51 +0000

    Added treesitter cpp tools for some quick code generation

---
+++ nvim/lua/user/lsp/settings/clangd.lua
@@ -0,0 +1,18 @@
+return {
+    capabilities = capabilities,
+    cmd = {
+        "clangd",
+        "--background-index",
+        "--clang-tidy",
+        "--header-insertion=iwyu",
+        "--completion-style=detailed",
+        "--function-arg-placeholders"
+    },
+    init_options = {
+        usePlaceholders = true,
+        completeUnimported = true,
+        clangdFileStatus = true
+    },
+    on_attach = on_attach,
+    flags = {debounce_text_changes = 150}
+}
