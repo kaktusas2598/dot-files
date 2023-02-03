af58ac174015afa679cfea89d8c63d97924a87cb
Nerijus <nerijus.vilcinskas@gmail.com>
2023-02-03 12:19:04 +0000

    Added custom code to toggle checkboxes/TODOs

--- nvim/.config/nvim/lua/user/remap.lua
+++ nvim/.config/nvim/lua/user/remap.lua
@@ -58,3 +58,6 @@ keymap("n", "<leader>t", ":TagbarToggle<CR>")
 --keymap('n', '<leader>fg', builtin.live_grep, {})
 --keymap('n', '<leader>fb', builtin.buffers, {})
 --keymap('n', '<leader>fh', builtin.help_tags, {})
+
+-- Toggle checkbox, maybe should only be enabled for certain filetypes like conf and markdown
+keymap("n", "<leader>m", ":lua require('toggle-checkbox').toggle()<CR>")
